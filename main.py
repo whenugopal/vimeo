fde0c2a11d11d3cd59316652c7faa8431e084b90
vg7venu <vg7venu@gmail.com>
2020-04-02 16:30:26 +0530

    main again changed

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ csv_f = csv.reader(f)
 for row in csv_f:
     print ""
     print ""
-    print f"<<<<<<<<<<<<<<<<<<<<<<<<< DONLOADING ::  {row[0]} >>>>>>>>>>>>>>>>>>>>>>>>>"
+    print row[0]
     os.system f'python vimeo-download.py --url "{row[1]}" --output "{row[0]}"'

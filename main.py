2e6ec223dcc3121c130643d4d1346da1636bd00b
vg7venu <vg7venu@gmail.com>
2020-04-02 16:34:51 +0530

    changed some stull

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import csv
 import os
 
-f = open('videos.csv')
+f = open("videos.csv")
 csv_f = csv.reader(f)
 
 for row in csv_f:
     print ""
     print ""
     print row[0]
-    os.system 'python vimeo-download.py --url '+row[1]+' --output '+row[0]
+    os.system "python vimeo-download.py --url "+row[1]+" --output "+row[0]
